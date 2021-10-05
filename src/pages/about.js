3b983588c3ba6f62854f01b32313dae841e10433
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2021-10-05 12:56:34 +0300

    add layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout';
 const AboutPage = () => {
     return (
-        <main>
-            <title>AboutPage</title>
-            <h1>Welcome to my Gatsby site!</h1>
-            <Link to='/'></Link>
-            <p>I'm making this by following the Gatsby Tutorial.</p>
-        </main>
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <Link to="/about">About</Link>
-      <h1>Welcome to my Gatsby site!</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 
