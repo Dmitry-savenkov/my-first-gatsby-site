8d08bf19b0443a66fc019a159f9a2231d8fcb973
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2021-10-05 12:43:39 +0300

    add pages.js link

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+const AboutPage = () => {
+    return (
+        <main>
+            <title>AboutPage</title>
+            <h1>Welcome to my Gatsby site!</h1>
+            <Link to='/'></Link>
+            <p>I'm making this by following the Gatsby Tutorial.</p>
+        </main>
+    )
+}
+
+export default AboutPage
